edfb45300c0da3b8834eef0509003e1238e18ced
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-07 02:58:50 -0400

    Minor changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
   selector: 'app-servers',
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>`,
+  // path to html template
   templateUrl: './servers.component.html',
+  // path to css
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
